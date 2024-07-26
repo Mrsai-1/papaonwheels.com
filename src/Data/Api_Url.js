6cd3b982373a42b786e220cf9f50634594c2e569
Mrsai-1 <saiphanindra8520@gmail.com>
2024-07-26 15:15:42 +0530

    for int

--- src/Data/Api_Url.js
+++ src/Data/Api_Url.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:8000"
+// export const API_URL = "http://localhost:8000"
 
-// const API_URL = "https://backend-papa.onrender.com"
+const API_URL = "https://backend-papa-1.onrender.com"
